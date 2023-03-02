4fa37445d9b884634a59d9ec5e7d5e2116fe768a
Anton <greek-64@mail.ru>
2023-03-02 21:02:44 +0300

    Jenkins

---
+++ src/main/java/ru/aston/controller/MyController.java
@@ -0,0 +1,12 @@
+package ru.aston.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+    @GetMapping("/hello")
+    public String showHello(){
+        return "hello";
+    }
+}
